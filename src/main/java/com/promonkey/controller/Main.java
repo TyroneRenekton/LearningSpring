192bda5f3a5e283ad821a9ab02bd1bfcddc2c2a5
humingshuai <humingshuai@meituan.com>
2018-01-25 16:25:57 +0800

    适配器模式

--- src/main/java/com/promonkey/controller/Main.java
+++ src/main/java/com/promonkey/controller/Main.java
@@ -9,6 +9,8 @@ import org.springframework.beans.factory.xml.XmlReaderContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.util.Assert;
 
+import java.util.HashMap;
+
 /**
  * @author hms
  * @date 2018/1/18 下午4:40
@@ -16,11 +18,14 @@ import org.springframework.util.Assert;
  */
 public class Main {
     public static void main(String[] args) {
-        ClassPathXmlApplicationContext applicationContext =
-                new ClassPathXmlApplicationContext("spring-context.xml");
-
-        Student student = (Student) applicationContext.getBean("student");
-
-        applicationContext.destroy();
+//        ClassPathXmlApplicationContext applicationContext =
+//                new ClassPathXmlApplicationContext("spring-context.xml");
+//
+//        Student student = (Student) applicationContext.getBean("student");
+//
+//        applicationContext.destroy();
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(1, 2);
+        map.remove(2);
     }
 }
